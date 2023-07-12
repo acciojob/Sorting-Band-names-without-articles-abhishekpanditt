6836751fa452635b3fca534f3773203370baaa3b
abhishekpanditt <123313123+abhishekpanditt@users.noreply.github.com>
2023-07-12 16:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+let list="";
+for(let element of touristSpots.sort((a, b) => {
+    const strip = (bandName) => bandName.replace(/^(a |an |the )/i, '');
+    return strip(a)-strip(b);
+  })){
+
+	 list+="<li>"+element+"</li>";
+
+}
+
+document.getElementById("band").innerHTML =list;
